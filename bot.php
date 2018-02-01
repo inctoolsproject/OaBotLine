4f74ed1a0c38d7664c29ee56b9e47f9adbc3664c
akununtukbelajar123 <34611544+akununtukbelajar123@users.noreply.github.com>
2018-02-01 16:38:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -343,7 +343,21 @@ if($message['type']=='text') {
             )
         );
     }
-	
+	if ($command == '/keluar') {
+		$push = array(
+					'to' => $groupId,									
+					'messages' => array(
+						array(
+								'type' => 'text',					
+								'text' => 'here is my poo...'
+							)
+					)
+				);
+						
+		
+		$client->pushMessage($push);
+        $psn = $client->leaveGroup($groupId);
+		}
 	}
 if (isset($balas)) {
     $result = json_encode($balas);

9e3bf6b883956fcc82f90370bb5d99c40655e353
Muhammad Raihan Permadi <32005026+rhnprmd@users.noreply.github.com>
2018-09-14 18:53:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -628,10 +628,9 @@ function zodiak($keyword) {
     return $result;
 }
 #-------------------------[Function]-------------------------#
-//show menu, saat join dan command,menu
-if ($type == 'join' || $command == 'Help') {
-    $text = "Makasih dh invite aku ke grup kak!! Ketik Help untuk gunain aku\n";
-    $text .= "Keyword GalaxySMP ~~~\n";
+//show menu, saat join dan command /menu
+if ($command == '/menu') {
+    $text .= "Keyword BedBodtzs ~~~\n";
     $text .= "> /anime-syn [text]\n";
     $text .= "> /anime [text]\n";
     $text .= "> /manga-syn [text]\n";
@@ -652,9 +651,24 @@ if ($type == 'join' || $command == 'Help') {
     $text .= "> /qiblat [namakota]\n";
     $text .= "> /playstore [namaapk]\n";
     $text .= "> /myinfo\n";
-    $text .= "> /creator\n";
+    $text .= "> /owner\n";
     $text .= "> /about\n";
     $text .= "> /bantuan\n";
+    $balas = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $text
+            )
+        )
+    );
+}
+if ($type == 'join') {
+    $text = "Terimakasih Telah invite bot ini ke group kalian
+    Jika ada saran dan masukan kalian tinggal chat owner kami
+    Ketik /owner untuk mendapatkan contact owner, Silahkan Ketik /bantuan untuk melihat
+    Menu Bot kami :)";
     $balas = array(
         'replyToken' => $replyToken,
         'messages' => array(
@@ -696,6 +710,7 @@ Status: '.$profil->statusMessage.'
 
 Picture: '.$profil->pictureUrl.'
 
+
 ====[InfoProfile]===='
 									)
 							)
@@ -703,6 +718,102 @@ Picture: '.$profil->pictureUrl.'
 				
 	}
 }
+if ($command == '/infome') { 
+     
+        $balas = array( 
+            'replyToken' => $replyToken, 
+            'messages' => array( 
+                array ( 
+                        'type' => 'template', 
+                          'altText' => 'About Owner', 
+                          'template' =>  
+                          array ( 
+                            'type' => 'buttons', 
+                            'thumbnailImageUrl' => .$profil->pictureUrl., 
+                            'imageAspectRatio' => 'rectangle', 
+                            'imageSize' => 'cover', 
+                            'imageBackgroundColor' => '#FFFFFF', 
+                            'title' => .$profil->displayName., 
+                            'text' => .$profil->statusMessage., 
+                            'actions' =>  
+                            array ( 
+                              0 =>  
+                              array ( 
+                                'type' => 'uri', 
+                                'label' => 'Edit', 
+                                'uri' => 'line://nv/profile',
+                              ), 
+                            ), 
+                          ), 
+                        ) 
+            ) 
+        ); 
+    }
+	}
+if ($command == '/infome2') { 
+     
+        $balas = array( 
+            'replyToken' => $replyToken, 
+            'messages' => array( 
+                array ( 
+                        'type' => 'template', 
+                          'altText' => 'About Owner', 
+                          'template' =>  
+                          array ( 
+                            'type' => 'buttons', 
+                            'thumbnailImageUrl' => '.$profil->pictureUrl.', 
+                            'imageAspectRatio' => 'rectangle', 
+                            'imageSize' => 'cover', 
+                            'imageBackgroundColor' => '#FFFFFF', 
+                            'title' => '.$profil->displayName.', 
+                            'text' => '.$profil->statusMessage.', 
+                            'actions' =>  
+                            array ( 
+                              0 =>  
+                              array ( 
+                                'type' => 'uri', 
+                                'label' => 'Edit', 
+                                'uri' => 'line://nv/profile',
+                              ), 
+                            ), 
+                          ), 
+                        ) 
+            ) 
+        ); 
+    }
+	}
+if ($command == '/infome3') { 
+     
+        $balas = array( 
+            'replyToken' => $replyToken, 
+            'messages' => array( 
+                array ( 
+                        'type' => 'template', 
+                          'altText' => 'About Owner', 
+                          'template' =>  
+                          array ( 
+                            'type' => 'buttons', 
+                            'thumbnailImageUrl' => $profil->pictureUrl, 
+                            'imageAspectRatio' => 'rectangle', 
+                            'imageSize' => 'cover', 
+                            'imageBackgroundColor' => '#FFFFFF', 
+                            'title' => $profil->displayName, 
+                            'text' => $profil->statusMessage, 
+                            'actions' =>  
+                            array ( 
+                              0 =>  
+                              array ( 
+                                'type' => 'uri', 
+                                'label' => 'Edit', 
+                                'uri' => 'line://nv/profile', 
+                              ), 
+                            ), 
+                          ), 
+                        ) 
+            ) 
+        ); 
+    }
+	}
 //pesan bergambar
 if ($message['type'] == 'text') {
     if ($command == '/def') {
@@ -1522,9 +1633,9 @@ if($message['type']=='text') {
 										          1 => 
 										          array (
 													'type' => 'postback',
-													'label' => 'About',
+													'label' => 'Owner',
 													'data' => 'action=add&itemid=111',
-													'text' => 'Ketik /about'
+													'text' => '/owner'
 										          ),
 										          2 => 
 										          array (
@@ -1544,6 +1655,38 @@ if($message['type']=='text') {
 	}
 	
 }
+if ($command == '/owner') { 
+     
+        $balas = array( 
+            'replyToken' => $replyToken, 
+            'messages' => array( 
+                array ( 
+                        'type' => 'template', 
+                          'altText' => 'About Owner', 
+                          'template' =>  
+                          array ( 
+                            'type' => 'buttons', 
+                            'thumbnailImageUrl' => 'https://image.prntscr.com/image/K0b2P-S6RO6fzFqOVwkgtw.jpg', 
+                            'imageAspectRatio' => 'rectangle', 
+                            'imageSize' => 'cover', 
+                            'imageBackgroundColor' => '#FFFFFF', 
+                            'title' => 'Muhammad Raihan Permadi', 
+                            'text' => 'Creator Panda', 
+                            'actions' =>  
+                            array ( 
+                              0 =>  
+                              array ( 
+                                'type' => 'uri', 
+                                'label' => 'Contact', 
+                                'uri' => 'https://line.me/ti/p/~rhnprmd', 
+                              ), 
+                            ), 
+                          ), 
+                        ) 
+            ) 
+        ); 
+    }
+	}
 if (isset($balas)) {
     $result = json_encode($balas);
 //$result = ob_get_clean();

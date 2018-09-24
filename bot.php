160609e5d46e1a5a5b50395b14255c50c80530ac
Muhammad Raihan Permadi <32005026+rhnprmd@users.noreply.github.com>
2018-09-24 17:16:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -567,10 +567,7 @@ if($message['type']=='text') {
 if (isset($balas)) {
     $result = json_encode($balas);
 //$result = ob_get_clean();
-
     file_put_contents('./balasan.json', $result);
-
-
     $client->replyMessage($balas);
 }
 ?>
